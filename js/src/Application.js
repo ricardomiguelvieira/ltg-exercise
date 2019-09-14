2cb0fd6dd76dd92c06b45cd9314145b30a66e643
ricardom_vieira <ricardo@getmygrades.co.uk>
2019-09-14 19:20:51 +0100

    refactoring

--- js/src/Application.js
+++ js/src/Application.js
@@ -1,12 +1,19 @@
+/* eslint-disable max-len */
+/* eslint-disable no-mixed-operators */
 /* eslint-disable consistent-return */
 class Application {
   /* Get total price of products in basket */
   static getTotalPrice(basket, discounts) {
     if (this.isEmptyBasket(basket)) return 0;
     let total = 0;
+    let discountedPrice = 0;
     Object.keys(basket).forEach((productId) => {
-      const discount = basket[productId].discounts.length ? this.getDiscount(basket[productId], discounts) : 0;
-      total += (basket[productId].price * basket[productId].quantity) - discount;
+      if (basket[productId].discounts.length) {
+        discountedPrice = this.getDiscount(basket[productId], discounts);
+        total += discountedPrice;
+      } else {
+        total += basket[productId].price * basket[productId].quantity;
+      }
     });
     return total;
   }
@@ -16,89 +23,51 @@ class Application {
   }
 
   static getDiscount(product, discounts) {
+    let totalDiscount = 0;
+    let productPrice = product.price;
+    const productQuantity = product.quantity;
 
     /* A hash containing all discount types */
     const discountTypes = {
       percent: this.getPercentDiscount,
       absolute: this.getAbsoluteDiscount,
-      buyNGetMFree: this.buyNGetMFree,
+      buyNGetMFree: this.getBuyNGetNFreeDiscount,
+      nForThePriceOfM: this.getNForThePriceOfM,
     };
-    let totalDiscount = 0;
+
+
     const discountsList = Object.keys(discounts);
 
     discountsList.forEach((discountCode) => {
       const currentDiscountType = discounts[discountCode].type;
       if (this.discountHasMinimumQualifier(product, discounts[discountCode])) {
-        totalDiscount += discountTypes[currentDiscountType](product, discounts[discountCode]);
+        totalDiscount += discountTypes[currentDiscountType](productPrice, discounts[discountCode], product);
       }
-    })
-    return totalDiscount;
+      productPrice = (productPrice * productQuantity) - totalDiscount;
+    });
+    return productPrice;
   }
 
   static discountHasMinimumQualifier(product, discount) {
     return product.quantity >= discount.min;
   }
 
-  static getPercentDiscount(product, discount) {
+  static getPercentDiscount(productPrice, discount) {
     const decimal = discount.value / 100;
-    return product.price * decimal;
+    return productPrice * decimal;
+  }
+
+  static getAbsoluteDiscount(productPrice, discount) {
+    return discount.value;
+  }
+
+  static getBuyNGetNFreeDiscount(productPrice, discount, product) {
+    return discount.n * product.price - (discount.n - discount.m) * product.price;
+  }
+
+  static getNForThePriceOfM(productPrice, discount, product) {
+    return (product.price * discount.n) - (product.price * discount.m);
   }
 }
 
 module.exports = Application;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   /* Get comulative discount */
-//   static getComulativeDiscount(basket, discounts) {
-//     const basketItem = Object.keys(basket);
-//     const discountItems = Object.keys(discounts);
-//     let total = basket[basketItem].price;
-
-//     /* If there isn't a comulative discount, callback to getPercentageDiscount() function */
-//     if (discountItems.length === 1) return this.getPercentDiscount(basket, discounts);
-
-//     /* If no discount is present, callback to getBasePrice() function */
-//     if (discountItems.length < 1) return this.getBasePrice(basket);
-
-//     /* Loop through each discount and applies to product price */
-//     discountItems.forEach((item) => {
-//       if (discounts[item].type === 'percent') {
-//         const decimal = (100 - discounts[item].value) / 100;
-//         total *= decimal;
-//       } else {
-//         total -= discounts[item].value;
-//       }
-//     });
-//     return Math.round(total * 100) / 100;
-//   }
-
-//   /* Get absolute discount */
-//   static getAbsoluteDiscount(basket, discounts) {
-//     const basketItem = Object.keys(basket);
-//     const discountItems = Object.keys(discounts);
-//     const discountType = discounts[discountItems].type;
-
-//     if (discountType === 'percent') {
-//       return this.getPercentDiscount(basket, discounts);
-//     }
-//     return (basket[basketItem].price * basket[basketItem].quantity) - discounts[discountItems].value;
-//   }
-// }
-
-
