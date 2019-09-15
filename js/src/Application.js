88d30163487546e848158243ae0b216b68bc690d
ricardom_vieira <ricardo@getmygrades.co.uk>
2019-09-15 15:46:26 +0100

    refactoring

--- js/src/Application.js
+++ js/src/Application.js
@@ -1,13 +1,23 @@
 class Application {
-  /* Get total price of products in basket */
+  /**
+   * @function
+   * @name getTotalPrice
+   * @static
+   * @param {Object} basket The products in the basket.
+   * @param {Object} discounts The discounts applied to the products.
+   * @returns {number} The total price of the basket (including any discounts).
+   * @description Main function which is called to calculate the total price of the basket.
+   * Loops through each product in the basket.
+   * Increments total with product's base price times quantity in basket if isEmptyBasket() false.
+   * Increments total with price of product with discounts after calculating in getDiscount().
+   */
+
   static getTotalPrice(basket, discounts) {
     if (this.isEmptyBasket(basket)) return 0;
     let total = 0;
-    let discountedPrice = 0;
     Object.keys(basket).forEach((productId) => {
       if (basket[productId].discounts.length) {
-        discountedPrice = this.getDiscount(basket[productId], discounts);
-        total += discountedPrice;
+        total += this.getDiscount(basket[productId], basket[productId].discounts, discounts);
       } else {
         total += basket[productId].price * basket[productId].quantity;
       }
@@ -15,57 +25,145 @@ class Application {
     return total;
   }
 
+  /**
+   * @function
+   * @name isEmptyBasket
+   * @static
+   * @param {Object} basket The products in the basket.
+   * @returns {boolean}
+   * @description Returns true if basket is empty.
+   */
   static isEmptyBasket(basket) {
     return Object.keys(basket).length === 0;
   }
 
-  static getDiscount(product, discounts) {
-    let totalDiscount = 0;
-    let productPrice = product.price;
-    const productQuantity = product.quantity;
+  /**
+   * @function
+   * @name getDiscount
+   * @static
+   * @param {Object} product A product in the basket.
+   * @param {Array} appliedDiscountCodes Discount codes applied to the products.
+   * @param {Object} appliedDiscountsDetails Details of discounts' applied to the products
+   * @returns {number} The total price of the product (including any discounts).
+   * @description Calculates discounts for a product.
+   * Loop through discount codes applied to product.
+   * Verify if code can be applied by calling function discountHasMinimumQualifier().
+   * productTotalPrice is updated with the new product price every time a discount is applied.
+   * productTotalPrice is passed as argument to the functions at every loop. This way,
+   * functions calculate comulative discounts based on a new (already discounted) price.
+   */
+  static getDiscount(product, appliedDiscountCodes, appliedDiscountsDetails) {
+    let productTotalPrice = product.price * product.quantity;
 
-    /* A hash containing all discount types */
-    const discountTypes = {
+    // A hash containing all discount functions.
+    const calculateDiscount = {
       percent: this.getPercentDiscount,
       absolute: this.getAbsoluteDiscount,
-      buyNGetMFree: this.getBuyNGetNFreeDiscount,
+      buyNGetMFree: this.getBuyNGetMFreeDiscount,
       nForThePriceOfM: this.getNForThePriceOfM,
     };
 
-
-    const discountsList = Object.keys(discounts);
-
-    discountsList.forEach((discountCode) => {
-      const currentDiscountType = discounts[discountCode].type;
-      if (this.discountHasMinimumQualifier(product, discounts[discountCode])) {
-        totalDiscount = (discountTypes[currentDiscountType](productPrice, discounts[discountCode], product));
+    appliedDiscountCodes.forEach((code) => {
+      const discountDetails = appliedDiscountsDetails[code];
+      const discountType = discountDetails.type;
+      if (this.discountHasMinimumQualifier(product, discountDetails)) {
+        // Calls the appropriate discount function.
+        productTotalPrice -= (calculateDiscount[discountType](productTotalPrice, discountDetails, product));
       }
-      productPrice = (productPrice * productQuantity) - totalDiscount;
     });
-    return productPrice;
+    return productTotalPrice;
   }
 
+  /**
+   * @function
+   * @name discountHasMinimumQualifier
+   * @static
+   * @param {Object} product A product in the basket.
+   * @param {Object} discount Details of discount applied to the product.
+   * @returns {boolean} True if quantity of product in basket >= minimum quantity to apply discount.
+   * @description Verify if product is eligible for discount.
+   */
   static discountHasMinimumQualifier(product, discount) {
     return product.quantity >= discount.min;
   }
 
-  static getPercentDiscount(productPrice, discount) {
+  /**
+   * @function
+   * @name getPercentDiscount
+   * @static
+   * @param {number} productTotalPrice Total price of product (with discounts if already applied).
+   * @param {Object} discount Details of discount applied to the product.
+   * @returns {number} Amount of discount to subtract to product's total price.
+   * @description Calculates a percentage discount.
+   */
+  static getPercentDiscount(productTotalPrice, discount) {
     const decimal = discount.value / 100;
-    return productPrice * decimal;
+    return productTotalPrice * decimal;
   }
 
-  static getAbsoluteDiscount(productPrice, discount) {
-    return discount.value;
+  /**
+   * @function
+   * @name getAbsoluteDiscount
+   * @static
+   * @param {Object} discount Details of discount applied to the product.
+   * @param {Object} product A product in the basket.
+   * @returns {number} Amount of discount to subtract to product's total price.
+   * @description Calculates an absolute discount.
+   */
+  static getAbsoluteDiscount(productTotalPrice, discount, product) {
+    return product.quantity * discount.value;
   }
 
-  static getBuyNGetNFreeDiscount(productPrice, discount, product) {
+  /**
+   * @function
+   * @name getBuyNGetMFreeDiscount
+   * @static
+   * @param {number} productTotalPrice Total price of product (with discounts if already applied).
+   * @param {Object} discount Details of discount applied to the product.
+   * @param {Object} product A product in the basket.
+   * @returns {number} Amount of discount to subtract to product's total price.
+   * @description Calculates a Buy N Get Me Free discount.
+   */
+  static getBuyNGetMFreeDiscount(productTotalPrice, discount, product) {
+    // Number of times the discount will be applied based on product quantity in the basket and
+    // minimum quantity for discount elegibility.
+    const timesDiscountsApply = Math.floor(product.quantity / discount.min); // E.g. 7 / 3 = 2
+
+    /**
+     * A = (discount.n * (productTotalPrice / product.quantity)
+     ** Price of product based on N quantity (N quantity is quantity to effectively be paid).
+     * B = (discount.n - discount.m) * (productTotalPrice / product.quantity)
+     ** Price of product based on M quantity free (M quantity is quantity to effectively be free).
+     * Subtract B from A results in effective amount of discount.
+     * Then multiply it to number of times the discount is to be applied.
+     */
+    return (((discount.n * (productTotalPrice / product.quantity)) - ((discount.n - discount.m) * (productTotalPrice / product.quantity)))) * timesDiscountsApply;
+  }
+
+  /**
+   * @function
+   * @name getNForThePriceOfM
+   * @static
+   * @param {number} productTotalPrice Total price of product (with discounts if already applied).
+   * @param {Object} discount Details of discount applied to the product.
+   * @param {Object} product A product in the basket.
+   * @returns {number} Amount of discount to subtract to product's total price.
+   * @description Calculates a Get N for The Price of M discount.
+   */
+  static getNForThePriceOfM(productTotalPrice, discount, product) {
+    // Number of times the discount will be applied based on product quantity in the basket and
+    // minimum quantity for discount elegibility.
     const timesDiscountsApply = Math.floor(product.quantity / discount.min);
-    return (((discount.n * product.price) - ((discount.n - discount.m) * product.price))) * timesDiscountsApply;
-  }
 
-  static getNForThePriceOfM(productPrice, discount, product) {
-    const timesDiscountsApply = Math.floor(product.quantity / discount.min);
-    return ((product.price * discount.n) - (product.price * discount.m)) * timesDiscountsApply;
+    /**
+     * A = (discount.n * (productTotalPrice / product.quantity)
+     ** Price of product based on N quantity (N quantity is quantity of product to take home).
+     * B = (discount.m * (productTotalPrice / product.quantity)
+     ** Price of product based on M quantity (M quantity is quantity to effectively be paid).
+     * Subtract B from A results in effective amount of discount.
+     * Then multiply it to number of times the discount is to be applied.
+    */
+    return ((discount.n * (productTotalPrice / product.quantity)) - (discount.m * (productTotalPrice / product.quantity))) * timesDiscountsApply;
   }
 }
 
--- js/src/run_this.js
+++ js/src/run_this.js
@@ -2,24 +2,18 @@ const app = require('../src/Application.js');
 
 const discounts = {
   'cd29ba8c-faf2-4493-9b6b-4b339310d82d': {
-    type: 'percent',
-    value: 10,
-    min: 1,
-  },
-  '6cb609e2-818e-40bb-9b29-8799ba328232': {
-    type: 'percent',
-    value: 5,
-    min: 1,
+    type: 'buyNGetMFree',
+    n: 2, // buy 2 ...
+    m: 1, // ... get 1 free
+    min: 3,
   },
 };
 const basket = {
   'c08200af-0fa9-45e3-a6a0-cb7bd6696d4e': {
     price: 10.0,
-    quantity: 1,
-    discounts: [
-      'cd29ba8c-faf2-4493-9b6b-4b339310d82d',
-      '6cb609e2-818e-40bb-9b29-8799ba328232',
-    ],
+    quantity: 3,
+    discounts: ['cd29ba8c-faf2-4493-9b6b-4b339310d82d'],
   },
 };
+
 app.getTotalPrice(basket, discounts);
